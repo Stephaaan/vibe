4a5a930358a4b1d6b34ce87f5d8aef5f816c0acc
Stephaaan <stefan.rothmajer@akademiasovy.sk>
2019-03-06 06:50:06 +0100

    channels implementation in progress

--- src/Channel.js
+++ src/Channel.js
@@ -3,7 +3,20 @@ import React, {Component} from "react";
 class Channel extends Component{
 	render(){
 		return (
-				<div className="channel_background">skuska</div>
+				<div className="channel_background">
+					<div className="channel_section channel_name_section">
+						meno
+					</div>
+					<div className="channel_section channel_listeners_section">
+						Listeners: 1653
+					</div>
+					<div className="channel_section channel_audio_section">
+						Neni zatial audio
+					</div>
+					<div className="channel_section channel_settings_section">
+						nic nenastavujeme
+					</div>
+				</div>
 			);
 	}
 }
